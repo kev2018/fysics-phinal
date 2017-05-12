b0c9adb34bd1e81eff7303326e49eb7119b5bc6d
kev2018 <8kludwig@stpatrick.org>
2017-05-12 14:00:06 -0500

    Add files via upload

---
+++ fysicsphinal.js
@@ -0,0 +1,14 @@
+function distance(v,t){
+  return v*t;
+function velocity(d,t){
+  return d/t;
+}
+function time(d,v){
+  return d/v;
+}
+function acceleration(vi,vf,t){
+  return (vi-vf)/t;
+}
+function properDistance(vint,vfin,time){
+  return (acceleration(vint,vfin,time)*t*t)/2+i*t
+}
